7a7b3210c6742c7d537d2240b4ad45043a9e98e2
Zack Scholl <zack.scholl@gmail.com>
2022-03-18 15:18:09 +0000

    use modified time

--- src/filecreated/filecreated_linux.go
+++ src/filecreated/filecreated_linux.go
@@ -9,7 +9,7 @@ import (
 func FileCreated(fname string) time.Time {
 	finfo, _ := os.Stat(fname)
 	stat_t := finfo.Sys().(*syscall.Stat_t)
-	return timespecToTime(stat_t.Ctim)
+	return timespecToTime(stat_t.Mtim)
 }
 
 func timespecToTime(ts syscall.Timespec) time.Time {
